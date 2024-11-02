e8fae9dd5074447681ed49fd5f331fb09d010800
thales0591 <thales0591@gmail.com>
2024-11-02 16:49:21 -0300

    :card_file_box: raw: make react-router inital configs

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
 import './App.css'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+import Home from './pages/Home/Home'
+import About from './pages/About/About'
 
 function App() {
   return (
     <>
       <div>
-        <h1>MiniBlog</h1>
+        <BrowserRouter>
+          <div className='container'>
+            <Routes>
+              <Route path='/' element={<Home/>}/>
+              <Route path='/about' element={<About/>}/>
+            </Routes>
+          </div>
+        </BrowserRouter>
       </div>
     </>
   )
