428ec021a2a01a3fb09c595aaad93985232a650b
thales0591 <thales0591@gmail.com>
2024-11-04 13:35:00 -0300

    :sparkles: feat: create register authentication using firebase resources

--- src/hooks/useAuthentication.jsx
+++ src/hooks/useAuthentication.jsx
@@ -1,27 +1,77 @@
+import { db } from "../firebase/config.js"
+
 import {
-    getAuth,
-    createUserWithEmailAndPassword,
-    signInWithEmailAndPassword,
-    updateProfile,
-    signOut
-} from "firebase/auth"
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  updateProfile,
+  signOut,
+} from "firebase/auth";
 
-import { useState, useEffect } from "react"
+import { useState, useEffect } from "react";
 
-export const userAuthentication = () => {
-    const [error, setError] = useState(null)
-    const [loading, setLoading] = useState(null)
+export const useAuthentication = () => {
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(null);
 
-    //cleanup
-    //deal with memory leak
+  //cleanup
+  //deal with memory leak
 
-    const [cancelled, setCancelled] = useState(false)
+  const [cancelled, setCancelled] = useState(false);
 
-    const auth = getAuth()
+  const auth = getAuth();
 
-    function checkIfIsCancelled() {
-        if(cancelled) {
-            return
-        }
+  function checkIfIsCancelled() {
+    if (cancelled) {
+      return;
     }
-}
+  }
+
+  const createUser = async (data) => {
+    checkIfIsCancelled();
+
+    setLoading(true);
+    setError(null)
+
+    try {
+      const { user } = await createUserWithEmailAndPassword(
+        auth,
+        data.email,
+        data.password
+      );
+
+      await updateProfile(user, {
+        displayName: data.displayName,
+      });
+      setLoading(false);
+      return user;
+    } catch (error) {
+      console.log(error.message);
+      console.log(typeof error.message);
+
+      let systemErrorMessage
+
+      if(error.message.includes("Password")) {
+        systemErrorMessage = "A senha precisa conter pelo menos 6 caracteres."
+      } else if (error.message.includes("e-mail-already")) {
+        systemErrorMessage = "E-mail já cadastrado. "
+      } else {
+        systemErrorMessage = "Ocorreu um erro, por favor tente mais tarde."
+      }
+      setLoading(false);
+      setError(systemErrorMessage)
+    }
+
+   
+  };
+
+  useEffect(() => {
+    return setCancelled(true)
+  }, [])
+  return {
+    auth,
+    createUser,
+    error,
+    loading,
+  };
+};
