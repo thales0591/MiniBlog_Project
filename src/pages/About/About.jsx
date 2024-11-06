9b9acc3dd1c42f2029d57a0f4261be4067b1c53f
thales0591 <thales0591@gmail.com>
2024-11-06 09:08:28 -0300

    :lock: feat: blocks pages that require authentication

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,9 +1,12 @@
+import { Link } from 'react-router-dom'
 import styles from './About.module.css'
 
 const About = () => {
   return (
-    <div>
-        <h1>About</h1>
+    <div className={styles.about}>
+        <h2>Sobre o Mini<span>Blog</span></h2>
+        <p>Este projeto consiste em um blog feito com React (Vite) no front-end e Firebase no backend.</p>
+        <Link to="/posts/create" className='btn'>Criar post</Link>
     </div>
   )
 }
