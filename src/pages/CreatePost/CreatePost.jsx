bd5a6635640681a83351debc9423d47a5b7a8f7b
thales0591 <thales0591@gmail.com>
2024-11-08 12:11:28 -0300

    :lock: feat: make user data validation when creating posts

--- src/pages/CreatePost/CreatePost.jsx
+++ src/pages/CreatePost/CreatePost.jsx
@@ -15,27 +15,40 @@ const CreatePost = () => {
 
   const {insertDocument, response} = useInsertDocument("posts")
 
+  const navigate = useNavigate()
+
   const handleSubmit = (e) => {
     e.preventDefault();
     setFormError("")
 
     //validate image url
-
+    try {
+      new URL(image)
+    } catch (error) {
+      return setFormError("A imagem precisa ser uma URL.")
+    }
     //create array tags
-
+    const arrayTags = tags.split(",").map((tag) => tag.trim().toLowerCase())
     //check all values
+
+    if(!title || !image || !body || !tags) {
+      return setFormError("Por favor, preencha todos os campos.")
+    }
 
     insertDocument({
       title,
       image,
       body, 
-      tags,
+      arrayTags,
       uid: user.uid,
       createdBy: user.displayName
     });
    
 
     // redirect to home page
+
+    navigate("/")
+   
   };
   return (
     <div className={styles.create_post}>
@@ -88,6 +101,7 @@ const CreatePost = () => {
         {!response.loading && <button className="btn">Cadastrar</button>}
         {response.loading && <button className="btn" disabled>Aguarde...</button>}
         {response.error && <p className="error">{response.error}</p>}
+        {formError && <p className="error">{formError}</p>}
       </form>
     </div>
   );
