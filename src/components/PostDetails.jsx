9165c1600c0dd63192f44b9549ff92ddf6d0ce30
thales0591 <thales0591@gmail.com>
2024-11-09 16:41:03 -0300

    :sparkles: feat: integrates the hook into the home to display posts

---
+++ src/components/PostDetails.jsx
@@ -0,0 +1,30 @@
+import styles from "./PostDetails.module.css"
+import { Link } from "react-router-dom"
+import PropTypes from 'prop-types'
+
+const PostDetails = ({post}) => {
+  return (
+    <div>
+        <img src={post.image} alt={post.title} />
+        <h2>{post.title}</h2>
+        <p>{post.createdBy}</p>
+        <div>
+            {post.arrayTags.map((tag) => (
+                <p key={tag}><span>#</span>{tag}</p>
+            ))}
+        </div>
+        <Link to={`/posts/${post.id}`} className="btn btn-outline">Ler</Link>
+    </div>
+  )
+}
+PostDetails.propTypes = {
+    post: PropTypes.shape({
+      image: PropTypes.string.isRequired,
+      title: PropTypes.string.isRequired,
+      createdBy: PropTypes.string.isRequired,
+      arrayTags: PropTypes.arrayOf(PropTypes.string).isRequired,
+      id: PropTypes.number.isRequired,
+    }).isRequired,
+  };
+
+export default PostDetails
