32f6e29ff106e6a1e0a4a8572177818cd8e2a9a0
thales0591 <thales0591@gmail.com>
2024-11-12 10:22:40 -0300

    :sparkles: feat: displays the posts brought up by the user id

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -10,22 +10,20 @@ const Dashboard = () => {
   const {user} = useAuthValue()
   const uid = user.uid
 
-  //post do usuario
-  const posts = []
+  const {documents: posts, loading} = useFetchDocuments("posts", null, uid)
   return (
     <div>
         <h1>Dashboard</h1>
         <p>Gerencie os seus posts</p>
-        {posts && posts.length === 0 ? (
+        {posts == null && (
           <div className={styles.noposts}>
             <p>Não foram encontrados posts :/</p>
             <Link to="/posts/create" className="btn">Criar primeiro post</Link>
-          </div>
-        ) : (
-          <div>
-            <p>Tem posts!</p>
-          </div>
-        )}
+          </div>)
+        }
+        {posts && posts.map((post) => (
+          <h3>{post.title}</h3>
+        ))}
     </div>
   )
 }
