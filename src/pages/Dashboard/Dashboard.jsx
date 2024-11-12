1d8e45eeb197c4f25ddf1927d5d2c5d8131b31a4
thales0591 <thales0591@gmail.com>
2024-11-12 10:36:30 -0300

    :heavy_plus_sign: dashboard: completes the structure of the dashboard

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -11,19 +11,45 @@ const Dashboard = () => {
   const uid = user.uid
 
   const {documents: posts, loading} = useFetchDocuments("posts", null, uid)
+
+  const deleteDocument = (id) => {
+
+  }
+
+  if (loading) {
+    return <p>Carregando...</p>
+  }
   return (
     <div>
         <h1>Dashboard</h1>
         <p>Gerencie os seus posts</p>
-        {posts == null && (
+        {posts == null ? (
           <div className={styles.noposts}>
             <p>Não foram encontrados posts :/</p>
             <Link to="/posts/create" className="btn">Criar primeiro post</Link>
-          </div>)
+          </div>
+          ) : (
+            <>
+            <div>
+              <span>Título</span>
+              <span>Ações</span>
+            </div>
+            {posts.map((post) => (
+              <div key={post.id}>
+                <p>{post.title}</p>
+                <div>
+                  <Link to={`/posts/${post.id}`} className="btn btn-outline">Ver</Link>
+                  <Link to={`/posts/edit/${post.id}`} className="btn btn-outline">Editar</Link>
+                  <button onClick={() => deleteDocument(post.id)} className="btn btn-outline btn-danger">
+                    Excluir
+                  </button>
+                </div>
+              </div>
+            ))}
+            </>
+          ) 
         }
-        {posts && posts.map((post) => (
-          <h3>{post.title}</h3>
-        ))}
+        
     </div>
   )
 }
