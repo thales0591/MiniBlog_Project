9b62cb6776ea7876630c52345bebb16745f40aaa
thales0591 <thales0591@gmail.com>
2024-11-12 15:00:13 -0300

    :sparkles: feat: create EditPost page

--- src/App.jsx
+++ src/App.jsx
@@ -20,6 +20,7 @@ import Dashboard from './pages/Dashboard/Dashboard'
 import CreatePost from './pages/CreatePost/CreatePost'
 import Search from './pages/Search/Search'
 import Post from './pages/Post/Post'
+import EditPost from './pages/EditPost/EditPost'
 
 function App() {
 
@@ -53,6 +54,7 @@ function App() {
                 <Route path='/login' element={!user ? <Login/> : <Navigate to="/"/>}/>
                 <Route path='/register' element={!user ? <Register/> : <Navigate to="/"/>}/>
                 <Route path='/dashboard' element={user ? <Dashboard/> : <Navigate to="/login"/>}/>
+                <Route path='/posts/edit/:id' element={user ? <EditPost/> : <Navigate to="/login"/>}/>
                 <Route path='/posts/create' element={user ? <CreatePost/> : <Navigate to="/login"/>}/>
               </Routes>
             </div>
--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,9 +1,15 @@
 import { useContext, createContext } from "react";
+import PropTypes from 'prop-types';
 
 const AuthContext = createContext();
 
 export const AuthProvider = ({ children, value }) => {
   return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
+};
+
+AuthProvider.propTypes = {
+  children: PropTypes.node.isRequired,
+  value: PropTypes.object.isRequired, 
 };
 
 export const useAuthValue = () => {
